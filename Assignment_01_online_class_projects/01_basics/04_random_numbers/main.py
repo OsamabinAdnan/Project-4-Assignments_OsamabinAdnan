abef6d1e88f3cad19091b6222732e59787c09b5d
OsamabinAdnan <binadnanosama@gmail.com>
2025-04-02 23:49:41 +0500

    Refactor random number generator to use configurable parameters for count and range

--- Assignment_01_online_class_projects/01_basics/04_random_numbers/main.py
+++ Assignment_01_online_class_projects/01_basics/04_random_numbers/main.py
@@ -1,13 +1,17 @@
-#  Random number generator
-
-# Here is an example run:
-
-# 45 79 61 47 52 10 16 83 19 12
-
+# Import random module for generating random numbers
 import random
 
-def main():
-    for i in range(1, 10):
-        print(random.randint(1, 100), end=' ')
+# Constants for number of random numbers to generate and their range
+n_numbers  = 10  # How many random numbers to generate
+min_value = 1    # Minimum value for random numbers
+max_value = 100  # Maximum value for random numbers
 
+
+def main():
+    # Generate and print n_numbers random integers between min_value and max_value
+    for i in range(n_numbers):
+        # Print each number followed by a space instead of newline
+        print(random.randint(min_value, max_value), end=' ')
+
+# Call the main function to execute the program
 main()
